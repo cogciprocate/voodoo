890cf58df08bb03fdc864d54ebcdff2593338d44
Nick Sanders <cogciprocate@gmail.com>
2017-10-15 14:47:02 -0700

    Fix voodoo_winit imports, clean up.

--- voodoo_winit/src/lib.rs
+++ voodoo_winit/src/lib.rs
@@ -7,22 +7,19 @@ use voodoo::{Result as VdResult, Instance, SurfaceKhr};
 
 #[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "openbsd"))]
 pub fn create_surface(instance: Instance, window: &WinitWindow) -> VdResult<SurfaceKhr> {
-    use std::mem;
     use winit::os::unix::WindowExt;
-    use ::{wl_display, wl_surface, Window};
+    use voodoo::{wl_display, wl_surface};
 
     let mut sb = SurfaceKhr::builder();
     unsafe {
-        if let (Some(display), Some(xlib_window)) = (window.get_xlib_display(), window.get_xlib_window()) {
-            // // FIXME Temporary until `vks` version 0.21+:
-            // let xw: Window = mem::transmute(xlib_window as i32);
-            // sb.xlib(display as _, xw);
+        if let (Some(display), Some(xlib_window)) = (window.get_xlib_display(),
+                window.get_xlib_window()) {
             sb.xlib(display as _, xlib_window as _);
-        } else if let (Some(display), Some(surface)) = (window.get_wayland_display(), window.get_wayland_surface()) {
+        } else if let (Some(display), Some(surface)) = (window.get_wayland_display(),
+                window.get_wayland_surface()) {
             sb.wayland(display as *mut wl_display, surface as *mut wl_surface);
         }
     }
-
     sb.build(instance)
 }
 
